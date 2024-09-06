19169b8b7720a24c20b342f56026a0e4cb2a0c75
Andres-RG <lrojas26@alumnos.uaq.mx>
2024-09-06 15:51:22 -0600

    act

--- MiceProject/02_scripts/01_procesamiento_y_filtrado_de_datos.R
+++ MiceProject/02_scripts/01_procesamiento_y_filtrado_de_datos.R
@@ -1,9 +1,11 @@
 # Librerias necesarias
 library(readr)
-
-##-------
-
-
+library(mlBioNets)
+##-----------------
+# Se cargan los datos procesados
+load(file = "03_out/data/asv_table.RData") # asv_table
+load(file = "03_out/data/asv_tabla_collapsed.RData") # asv_table_cllps
+##-----------------
 # asv abundance table
 asv_table <- as.data.frame(read_tsv("01_raw_data/counts.tsv"))
 asv_ids <- as.vector(asv_table[,1])
@@ -12,7 +14,7 @@ rownames(asv_table) <- asv_ids
 # Se puso el nombre de los asv en los renglones de la tabla, ya que venían 
 # agregados como una columna extra
 # Se guarda como archivo .RData
-save(asv_table, file = "03_out/data/asv_table.RData")
+# save(asv_table, file = "03_out/data/asv_table.RData")
 
 # asv metadata table
 asv_meta <- as.data.frame(read_tsv("01_raw_data/metadata.tsv"))
@@ -21,3 +23,36 @@ asv_meta <- as.data.frame(read_tsv("01_raw_data/metadata.tsv"))
 asv_pert <- as.data.frame(read_tsv("01_raw_data/perturbations.tsv"))
 
 # taxonomic table
+asv_taxa<-as.data.frame(read_tsv("01_raw_data/rdp_species.tsv"))
+##-----------------
+# Condensed table at genus level
+asv_table_cllps <- T_collapse(is_phyloseq = F,
+                              T_table = asv_taxa,
+                              O_table = asv_table,
+                              names_level = "Genus")
+# Junta todas las abundancias de los taxa y los guarda en un objeto, 
+# con todas las series de tiempo
+# Se guarda el objeto como .RData
+# save(asv_table_cllps, file = "03_out/data/asv_tabla_collapsed.RData")
+##-----------------
+# se separa la tabla por cada subject, que es cada raton
+# el raton 1 no tenpia perturbaciones, se toman unicamente los ratones 
+# 2, 3, 4 y 5
+## RATON 2
+subject2 <- asv_meta[which(asv_meta$subject == 2),]
+subject2 <- subject2[order(subject2$time), ]
+## RATON 3
+subject3 <- asv_meta[which(asv_meta$subject == 3),]
+subject3 <- subject3[order(subject3$time), ]
+## RATON 4
+subject4 <- asv_meta[which(asv_meta$subject == 4),]
+subject4 <- subject4[order(subject4$time), ]
+## RATON 5
+subject5 <- asv_meta[which(asv_meta$subject == 5),]
+subject5 <- subject5[order(subject5$time), ]
+##
+##
+# De cada raton, se separan las abundancias por tratamiento
+a <- subject2$time[1]
+b<-asv_pert[which(asv_pert$subject == 2),]$start[1]
+b<-subject2$time[which(subject2$time==b)-1]
