1fb579096ee80ff113f214670076cbd1786ad5b8
Andres-RG <lrojas26@alumnos.uaq.mx>
2024-09-24 10:50:07 -0600

    topological network analysis raton 3

---
+++ MiceProject/02_scripts/07_raton_3_topological_network_analysis.R
@@ -0,0 +1,271 @@
+# Topological network analysis raton_3 === --- ===
+##---------------------------
+# Se define la ruta
+setwd("MiceProject/")
+##---------------------------
+# Librerias necesarias
+library(devtools)
+library(readr)
+library(mlBioNets)
+library(igraph)
+library(minet)
+library(ggplot2)
+library(gridExtra)
+##---------------------------
+# Se cargan los datos procesados
+load(file = "03_out/data/asv_table.RData") # asv_table
+load(file = "03_out/data/asv_table_aggregate.RData") # asv_table_cllps
+load(file = "03_out/data/basal_period_mouse_3.RData")      # basal_subject3_aggregate
+load(file = "03_out/data/fatdiet_period_mouse_3.RData")    # fatdiet_subject3_aggregate
+load(file = "03_out/data/recovered1_period_mouse_3.RData") # recover1_subject3_aggregate
+load(file = "03_out/data/vancomycin_period_mouse_3.RData") # vancomycin_subject3_aggregate
+load(file = "03_out/data/recovered2_period_mouse_3.RData") # recover2_subject3_aggregate
+load(file = "03_out/data/gentamicin_period_mouse_3.RData") # gentamicin_subject3_aggregate
+load(file = "03_out/data/recovered3_period_mouse_3.RData") # recover3_subject3_aggregate
+load(file = "03_out/data/mice_multilayer_network_properties_mouse_3.RData") # mice_ml_properties
+##---------------------------
+ml_subject3 <- list(
+  basal_subject3_aggregate,
+  fatdiet_subject3_aggregate,
+  recover1_subject3_aggregate,
+  vancomycin_subject3_aggregate,
+  recover2_subject3_aggregate,
+  gentamicin_subject3_aggregate,
+  recover3_subject3_aggregate
+)
+##
+# Infiere las co-abundancias a partir de los datos de abundancia
+Nbasal_subject3      <- net_inference(basal_subject3_aggregate, "aracne")
+Nfatdiet_subject3    <- net_inference(fatdiet_subject3_aggregate, "aracne")
+Nrecover1_subject3   <- net_inference(recover1_subject3_aggregate, "aracne")
+Nvancomycin_subject3 <- net_inference(vancomycin_subject3_aggregate, "aracne")
+Nrecover2_subject3   <- net_inference(recover2_subject3_aggregate, "aracne")
+Ngentamicin_subject3 <- net_inference(gentamicin_subject3_aggregate, "aracne")
+Nrecover3_subject3   <- net_inference(recover3_subject3_aggregate, "aracne")
+##
+fatdiet_period_s3    <- list(Nbasal_subject3,
+                             Nfatdiet_subject3,
+                             Nrecover1_subject3)
+vancomyci_period_s3  <- list(Nrecover1_subject3,
+                             Nvancomycin_subject3,
+                             Nrecover2_subject3)
+gentamicin_period_s3 <- list(Nrecover2_subject3,
+                             Ngentamicin_subject3,
+                             Nrecover3_subject3)
+treatments <- c(1,2,3)
+##
+# Propiedades de las redes multicapa
+mice_ml_properties <- rbind(ml_properties(fatdiet_period_s3, treatments),
+                            ml_properties(vancomyci_period_s3, treatments),
+                            ml_properties(gentamicin_period_s3, treatments))
+##
+Stage = c(rep("Fat diet", length(fatdiet_period_s3)),
+          rep("Vancomycin", length(vancomyci_period_s3)),
+          rep("Gentamicin", length(gentamicin_period_s3))
+)
+mice_ml_properties <- cbind(mice_ml_properties, Stage)
+## Se genera un objeto que tiene las propiedades de las redes multicapa por 
+## cada tratamiento
+## Se guarda objeto .RData
+# save(mice_ml_properties, file = "03_out/data/mice_multilayer_network_properties_mouse_3.RData")
+##---------------------------
+names(mice_ml_properties)
+# Graficas
+## mean degree === === === ==
+plot_mean_degree <- ggplot(
+  mice_ml_properties,
+  aes(x = Treatments,
+      y = Mean_degree,
+      col = Stage) 
+) +
+  geom_point() + 
+  geom_smooth(
+    method = "lm",
+    se = F
+  ) +
+  labs(x = "Period",
+       y = "Mean degree",
+       tittle = "") +
+  theme_minimal() +
+  theme(
+    # linea del eje
+    axis.line = element_line(colour = "black", linewidth = 0.3),
+    # eje x
+    axis.text.x = element_text(angle = 0, hjust = 1, face = "bold"),
+    axis.title.x = element_text(size = 11, face = "bold"),
+    # eje y
+    axis.text.y = element_text(size = 9, face = "bold"),
+    axis.title.y = element_text(size = 11, face = "bold"),
+    # leyenda
+    legend.position = "right",  # Posición de la leyenda
+    legend.title = element_text(size = 10, face = "bold"),  # Título de la leyenda
+    legend.text = element_text(size = 10),  # Texto de la leyenda
+    legend.spacing = unit(0.5, "cm")
+  )
+## sd degree = === === === ==
+plot_sd_degree <- ggplot(
+  mice_ml_properties,
+  aes(x = Treatments,
+      y = sd_degree,
+      col = Stage) 
+) +
+  geom_point() + 
+  geom_smooth(
+    method = "lm",
+    se = F
+  ) +
+  labs(x = "Period",
+       y = "SD of degree",
+       tittle = "") +
+  theme_minimal() +
+  theme(
+    # linea del eje
+    axis.line = element_line(colour = "black", linewidth = 0.3),
+    # eje x
+    axis.text.x = element_text(angle = 0, hjust = 1, face = "bold"),
+    axis.title.x = element_text(size = 11, face = "bold"),
+    # eje y
+    axis.text.y = element_text(size = 9, face = "bold"),
+    axis.title.y = element_text(size = 11, face = "bold"),
+    # leyenda
+    legend.position = "right",  # Posición de la leyenda
+    legend.title = element_text(size = 10, face = "bold"),  # Título de la leyenda
+    legend.text = element_text(size = 10),  # Texto de la leyenda
+    legend.spacing = unit(0.5, "cm")
+  )
+## transitivity === === === =
+plot_transitivity <- ggplot(
+  mice_ml_properties,
+  aes(x = Treatments,
+      y = Clusterization,
+      col = Stage) 
+) +
+  geom_point() + 
+  geom_smooth(
+    method = "lm",
+    se = F
+  ) +
+  labs(x = "Period",
+       y = "Transitivity",
+       tittle = "") +
+  theme_minimal() +
+  theme(
+    # linea del eje
+    axis.line = element_line(colour = "black", linewidth = 0.3),
+    # eje x
+    axis.text.x = element_text(angle = 0, hjust = 1, face = "bold"),
+    axis.title.x = element_text(size = 11, face = "bold"),
+    # eje y
+    axis.text.y = element_text(size = 9, face = "bold"),
+    axis.title.y = element_text(size = 11, face = "bold"),
+    # leyenda
+    legend.position = "right",  # Posición de la leyenda
+    legend.title = element_text(size = 10, face = "bold"),  # Título de la leyenda
+    legend.text = element_text(size = 10),  # Texto de la leyenda
+    legend.spacing = unit(0.5, "cm")
+  )
+## edge density === === === =
+plot_edge_density <- ggplot(
+  mice_ml_properties,
+  aes(x = Treatments,
+      y = Edge_density,
+      col = Stage) 
+) +
+  geom_point() + 
+  geom_smooth(
+    method = "lm",
+    se = F
+  ) +
+  labs(x = "Period",
+       y = "Edge density",
+       tittle = "") +
+  theme_minimal() +
+  theme(
+    # linea del eje
+    axis.line = element_line(colour = "black", linewidth = 0.3),
+    # eje x
+    axis.text.x = element_text(angle = 0, hjust = 1, face = "bold"),
+    axis.title.x = element_text(size = 11, face = "bold"),
+    # eje y
+    axis.text.y = element_text(size = 9, face = "bold"),
+    axis.title.y = element_text(size = 11, face = "bold"),
+    # leyenda
+    legend.position = "right",  # Posición de la leyenda
+    legend.title = element_text(size = 10, face = "bold"),  # Título de la leyenda
+    legend.text = element_text(size = 10),  # Texto de la leyenda
+    legend.spacing = unit(0.5, "cm")
+  )
+## proportion of linked nodes ===
+plot_linked_nodes <- ggplot(
+  mice_ml_properties,
+  aes(x = Treatments,
+      y = Connected_nodes,
+      col = Stage) 
+) +
+  geom_point() + 
+  geom_smooth(
+    method = "lm",
+    se = F
+  ) +
+  labs(x = "Period",
+       y = "Proportion of linked nodes",
+       tittle = "") +
+  theme_minimal() +
+  theme(
+    # linea del eje
+    axis.line = element_line(colour = "black", linewidth = 0.3),
+    # eje x
+    axis.text.x = element_text(angle = 0, hjust = 1, face = "bold"),
+    axis.title.x = element_text(size = 11, face = "bold"),
+    # eje y
+    axis.text.y = element_text(size = 9, face = "bold"),
+    axis.title.y = element_text(size = 11, face = "bold"),
+    # leyenda
+    legend.position = "right",  # Posición de la leyenda
+    legend.title = element_text(size = 10, face = "bold"),  # Título de la leyenda
+    legend.text = element_text(size = 10),  # Texto de la leyenda
+    legend.spacing = unit(0.5, "cm")
+  )
+## modularity === === === ===
+plot_modularity <- ggplot(
+  mice_ml_properties,
+  aes(x = Treatments,
+      y = Modularity,
+      col = Stage) 
+) +
+  geom_point() + 
+  geom_smooth(
+    method = "lm",
+    se = F
+  ) +
+  labs(x = "Period",
+       y = "Modularity",
+       tittle = "") +
+  theme_minimal() +
+  theme(
+    # linea del eje
+    axis.line = element_line(colour = "black", linewidth = 0.3),
+    # eje x
+    axis.text.x = element_text(angle = 0, hjust = 1, face = "bold"),
+    axis.title.x = element_text(size = 11, face = "bold"),
+    # eje y
+    axis.text.y = element_text(size = 9, face = "bold"),
+    axis.title.y = element_text(size = 11, face = "bold"),
+    # leyenda
+    legend.position = "right",  # Posición de la leyenda
+    legend.title = element_text(size = 10, face = "bold"),  # Título de la leyenda
+    legend.text = element_text(size = 10),  # Texto de la leyenda
+    legend.spacing = unit(0.5, "cm")
+  )
+##
+# jpeg("03_out/plots/plot_mice_multilayer_network_analysis_mouse_3.jpeg",
+#      width = 9333, height = 3700, res = 800, units = "px")
+grid.arrange(plot_mean_degree,
+             plot_sd_degree,
+             plot_transitivity,
+             plot_edge_density,
+             plot_linked_nodes,
+             plot_modularity,
+             ncol = 3,
+             top = "Multilayer network analysis")
+# dev.off()
