cab8534435b1088ed023dcb4dc58e10770faf303
Andres-RG <lrojas26@alumnos.uaq.mx>
2024-10-07 11:51:44 -0600

    se hacen comentarios en el script

--- MiceProject/02_scripts/11_indices_de_diversidad_raton_2.R
+++ MiceProject/02_scripts/11_indices_de_diversidad_raton_2.R
@@ -1,8 +1,10 @@
 # Determinación de indices de diversidad alfa raton 2 === --- ===
 ##---------------------------
+# Se define la ruta de trabajo
+setwd("MiceProject/")
 # Librerias necesarias
 library(vegan)
-##---------------------------
+  ##---------------------------
 # Se cargan los datos procesados
 load(file = "03_out/data/basal_period_mouse_2.RData")      # basal_subject2_aggregate
 load(file = "03_out/data/fatdiet_period_mouse_2.RData")    # fatdiet_subject2_aggregate
@@ -11,6 +13,7 @@ load(file = "03_out/data/vancomycin_period_mouse_2.RData") # vancomycin_subject2
 load(file = "03_out/data/recovered2_period_mouse_2.RData") # recover2_subject2_aggregate
 load(file = "03_out/data/gentamicin_period_mouse_2.RData") # gentamicin_subject2_aggregate
 load(file = "03_out/data/recovered3_period_mouse_2.RData") # recover3_subject2_aggregate
+load(file = "03_out/data/mouse2.RData")                    # mouse2
 ##---------------------------
 # Se guarda todas las observaciones del raton 2
 mouse2 <- rbind(basal_subject2_aggregate,
@@ -20,53 +23,53 @@ mouse2 <- rbind(basal_subject2_aggregate,
                 recover2_subject2_aggregate,
                 gentamicin_subject2_aggregate,
                 recover3_subject2_aggregate)
-# save(mouse2, file = "03_out/mouse2.RData")
+# save(mouse2, file = "03_out/data/mouse2.RData")
 ##---------------------------
 # Indices de diversidad
 # simpson
-s_i <- apply(mouse2, 1, function(x) diversity(x, index = "simpson"))
-names(s_i) <- c()
-simpson_index <- data.frame(
-  time = seq(1,length(s_i),1),
-  simpson = s_i
+simpson_mouse2 <- apply(mouse2, 1, function(x) diversity(x, index = "simpson"))
+names(simpson_mouse2) <- c()
+simpson_index_mouse2 <- data.frame(
+  time = seq(1,length(simpson_mouse2),1),
+  simpson = simpson_mouse2
 )
-rownames(simpson_index) <- seq(1,length(s_i),1)
-simpson_index
+rownames(simpson_index_mouse2) <- seq(1,length(simpson_mouse2),1)
+simpson_index_mouse2
 # save(simpson_index, file = "03_out/data/index_diversity_simpson_mouse2.RData")
 # pielou
-pielou_index <- readRDS("~/Documents/maestria/MicroDynamics/MiceProject/01_raw_data/mice_data.RDS")
-pielou_index
-# save(pielou_index, file = "03_out/data/index_diversity_pielou_mouse2.RData")
+pielou_index_mouse2 <- readRDS("~/Documents/maestria/MicroDynamics/MiceProject/01_raw_data/mice_data.RDS")
+pielou_index_mouse2
+# save(pielou_index_mouse2, file = "03_out/data/index_diversity_pielou_mouse2.RData")
 # berger-parker index
-b_p_index <- apply(mouse2, 1,
+berger_parker_mouse2 <- apply(mouse2, 1,
                              function(x) max(x)/sum(x))
-names(b_p_index) <- c()
-berger_parker_index <- data.frame(
-  time = seq(1,length(b_p_index),1),
-  b_p = b_p_index
+names(berger_parker_mouse2) <- c()
+berger_parker_index_mouse2 <- data.frame(
+  time = seq(1,length(berger_parker_mouse2),1),
+  b_p = berger_parker_mouse2
 )
-rownames(berger_parker_index) <- seq(1,length(b_p_index),1)
-berger_parker_index
-# save(berger_parker_index, file = "03_out/data/index_diversity_berger_parker_mouse2.RData")
-# shannon
-sha_i <- apply(mouse2,
+rownames(berger_parker_index_mouse2) <- seq(1,length(berger_parker_mouse2),1)
+berger_parker_index_mouse2
+# save(berger_parker_index_mouse2, file = "03_out/data/index_diversity_berger_parker_mouse2.RData")
+# shannon normalizado
+shannon_mouse2 <- apply(mouse2,
                1,
                function(x) diversity(x, index = "shannon"))
 S <- apply(mouse2, 1, function(x) sum(x > 0))
-shannon_normalized <- sha_i / log(S)
-names(shannon_normalized) <- c()
-shannon_normalized_index <- data.frame(
-  time = seq(1,length(shannon_normalized),1),
-  shannon = shannon_normalized
+shannon_normalized_mouse2 <- shannon_mouse2 / log(S)
+names(shannon_normalized_mouse2) <- c()
+shannon_normalized_index_mouse2 <- data.frame(
+  time = seq(1,length(shannon_normalized_mouse2),1),
+  shannon = shannon_normalized_mouse2
 )
-rownames(shannon_normalized_index) <- seq(1,length(shannon_normalized),1)
-shannon_normalized_index
-# save(shannon_normalized_index, file = "03_out/data/index_diversity_shannon_normalized_mouse2.RData")
+rownames(shannon_normalized_index_mouse2) <- seq(1,length(shannon_normalized_mouse2),1)
+shannon_normalized_index_mouse2
+# save(shannon_normalized_index_mouse2, file = "03_out/data/index_diversity_shannon_normalized_mouse2.RData")
 # gini-simpson
-gini_simpson_index <- data.frame(
-  time = seq(1,length(s_i),1),
-  gini_simpson = 1 - simpson_index[,2]
+gini_simpson_index_mouse2 <- data.frame(
+  time = seq(1,length(simpson_mouse2),1),
+  gini_simpson = 1 - simpson_index_mouse2[,2]
 )
-rownames(gini_simpson_index) <- seq(1,length(s_i),1)
-gini_simpson_index
-# save(gini_simpson_index, file = "03_out/data/index_diversity_gini_simpson_mouse2.RData")
+rownames(gini_simpson_index_mouse2) <- seq(1,length(simpson_mouse2),1)
+gini_simpson_index_mouse2
+# save(gini_simpson_index_mouse2, file = "03_out/data/index_diversity_gini_simpson_mouse2.RData")
